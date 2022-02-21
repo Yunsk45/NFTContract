b727d0166dd077792b448c512b0bbf3467aa8de8
Farlonx <nisrecht@gmail.com>
2022-02-21 13:51:50 +0100

    Wrote second test

--- test/sample-test.js
+++ test/sample-test.js
@@ -39,7 +39,16 @@ describe("Artwork Smart Contract tests", function() {
     expect(await artwork.balanceOf(account1.address)).to.equal(1);
   });
 
-  it("tokenURI is set successfully", async () => { });
+  it("tokenURI is set successfully", async () => {
+    [account1, account2] = await ethers.getSigners();
 
+    const tokenURI_1 = "https://opensea-creatures-api.herokuapp.com/api/creature/1"
+    const tokenURI_2 = "https://opensea-creatures-api.herokuapp.com/api/creature/2"
+
+    const tx1 = await artwork.connect(account1).mint(tokenURI_1);
+    const tx2 = await artwork.connect(account2).mint(tokenURI_2);
+
+    expect(await artwork.tokenURI(0)).to.equal(tokenURI_1);
+    expect(await artwork.tokenURI(1)).to.equal(tokenURI_2);
 
 });
