2e35ad89eadc7be8b527962b1a4d9e7e6bb7a371
Farlonx <nisrecht@gmail.com>
2022-02-21 13:27:37 +0100

    Modifier and setting token URI function

--- contracts/Artwork.sol
+++ contracts/Artwork.sol
@@ -7,11 +7,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract Artwork is ERC721 {
    
    uint256 public tokenCounter;
+   mapping (uint256 => string) _tokenURIs;
 
    
    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
       tokenCounter = 0;
    }
+
+    modifier existing(uint256 tokenId) {
+       require(_exists(_tokenId),
+        "ERC721Metadata: URI set of nonexistent token"
+       );
+       _;
+    }
 
    function mint(string memory _tokenURI) public {
       _safeMint(msg.sender, tokenCounter);
@@ -19,4 +27,9 @@ contract Artwork is ERC721 {
 
       tokenCounter++;
    }
+
+   function _setTokenURI(uint256 _tokenId, string memory _tokenURI) internal virtual existing(_tokenId) {
+      tokenURIs[_tokenId] = _tokenURI;
+   }
+
 }
