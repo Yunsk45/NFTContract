9208c63a5fffcd24c42370943768ca575e1a2a5c
Farlonx <nisrecht@gmail.com>
2022-02-21 13:06:09 +0100

    Adding global counter

--- contracts/Artwork.sol
+++ contracts/Artwork.sol
@@ -6,7 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Artwork is ERC721 {
    
+   uint253 public tokenCounter;
+   
    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-      
+      tokenCounter = 0;
    }
 }
