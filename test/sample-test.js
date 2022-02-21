83eaf880f29230c992185c4b83a21e0a087f25e6
Farlonx <nisrecht@gmail.com>
2022-02-21 13:49:55 +0100

    Wrote first test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-
+/*
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
@@ -16,4 +16,30 @@ describe("Greeter", function () {
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+});
+  */
+
+describe("Artwork Smart Contract tests", function() {
+
+  let artwork;
+
+  this.beforeEach(async function () {
+    const Artwork = await ethers.getContractFactory("Artwork");
+    artwork = await Artwork.deploy("Artwork Contract", "ART");
+  });
+
+  it("minted NFT successfully", async function () {
+    [account1] = await ethers.getSigners();
+
+    expect(await artwork.balanceOf(account1.address)).to.equal(0);
+
+    const tokenURI = "https://opensea-creatures-api.herokuapp.com/api/creature/1";
+    const tx = await artwork.connect(account1).mint(tokenURI);
+
+    expect(await artwork.balanceOf(account1.address)).to.equal(1);
+  });
+
+  it("tokenURI is set successfully", async () => { });
+
+
 });
